06901145e103ffb4710652ea60dc891c73eb7e6c
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-02-24 20:38:08 +0530

    Update Nav.jsx

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./nav.css";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <div>
+        <a href="#" className="nav_logo">
+          Ray Legal
+        </a>
+      </div>
 
-export default Nav
+      <div className="permalinks">
+        <a href="#">HOME</a>
+        <a href="#about">ABOUT</a>
+        <a href="#services">SERVICES</a>
+        <a href="#contact">CONTACT</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
