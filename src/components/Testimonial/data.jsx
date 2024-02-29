c5e794a28d6ba0114d975d6833ee393618f7dde6
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-02-29 19:28:27 +0530

    Update data.jsx

--- src/components/Testimonial/data.jsx
+++ src/components/Testimonial/data.jsx
@@ -3,25 +3,25 @@ export const data = [
         id: 1,
         title: "John Doe",
         description:
-        "A really good job, all aspects of the project were followed step by step and with good results"
+        "I was very pleased with the service I received from this business. They were professional, efficient, and provided great results."
     },
     {
         id: 2,
         title: "Henry Clinton",
         description:
-        "A really good job, all aspects of the project were followed step by step and with good results"
+        "I was very pleased with the service I received from this business. They were professional, efficient, and provided great results."
     },
     {
         id: 3,
         title: "Henry Smith",
         description:
-        "A really good job, all aspects of the project were followed step by step and with good results"
+        "I was very pleased with the service I received from this business. They were professional, efficient, and provided great results."
     },
     {
         id: 4,
         title: "Ema Smith",
         description:
-        "A really good job, all aspects of the project were followed step by step and with good results"
+        "I was very pleased with the service I received from this business. They were professional, efficient, and provided great results."
     },
 ]
 
