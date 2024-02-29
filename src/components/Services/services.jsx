60cc1f7feb508d1878bbf8bdbb4be34ab8769060
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-02-29 18:50:03 +0530

    Update services.jsx

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import "./services.css"
 
-const services = () => {
+const Services = () => {
   return (
-    <div>services</div>
+    <section id='services'>
+      <h5>What We Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services_container'>
+        <article className='service'>
+          <div className='service_head'>
+            <h3>Legal Consultation</h3>
+          </div>
+
+          <p className='parag'>Our experienced lawyers provide expert legal advice tailored to your specific needs.</p>
+        </article>
+        {/* END OF Legal Consultation */}
+        <article className='service'>
+          <div className='service_head'>
+            <h3>Litigation Support</h3>
+          </div>
+          
+          <p className='parag'>We offer comprehensive support in handling all aspects of your legal disputes.</p>
+        </article>
+        {/* END OF Litigation Support */}
+        <article className='service'>
+          <div className='service_head'>
+            <h3>Contract Drafting</h3>
+          </div>
+
+          <p className='parag'>Get legally sound contracts drafted by our skilled team to protect your interests.</p>
+        </article>
+        {/* END OF Contract Drafting */}
+      </div>
+    </section>
   )
 }
 
-export default services
+export default Services
