a8353627212d2fb05cb3fc862bf6639172543b92
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-02-29 18:57:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header/header";
 import Nav from "./components/Navbar/Nav";
 import About from "./components/About/about";
-import services from "./components/Services/services";
+import Services from "./components/Services/services";
 import Testimonial from "./components/Testimonial/testimonial";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
