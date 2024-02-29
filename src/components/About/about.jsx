e26cea2f338cd216ef0c998c030eeb3d152b8573
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-02-29 18:58:50 +0530

    Update about.jsx

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,47 +1,49 @@
-import React from 'react'
-import './about.css';
-import ME from '../../assests/ME.jpg'
-import { FaAward } from 'react-icons/fa'
-import { FiUsers } from 'react-icons/fi'
-import { VscFolderLibrary } from 'react-icons/vsc'
+import React from "react";
+import "./about.css";
+import ME from "../../assests/photo1.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
-      <div className='container about_container'>
-        <div className="about_me">
+      <div className="container about_container">
           <div className="about_me-image">
             <img src={ME} alt="me" />
-          </div>
         </div>
-        <div className='about_content'>
+        <div className="about_content">
           <div className="about_cards">
-            <article className='about_card'>
-            <FaAward className='about_icon' />
+            <article className="about_card">
+              <FaAward className="about_icon" />
               <h5>Experience</h5>
               <small>0 years</small>
             </article>
-            <article className='about_card'>
-            <FiUsers className='about_icon' />
+            <article className="about_card">
+              <FiUsers className="about_icon" />
               <h5>Client</h5>
               <small>0</small>
             </article>
-            <article className='about_card'>
-            <VscFolderLibrary className='about_icon' />
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
               <h5>Projects</h5>
               <small>3+</small>
             </article>
           </div>
           <p>
-            Hello there! I'm Kirti Kumar, a passionate and creative Full Stack Web Developer enthusiast with a strong desire to make a meaningful impact through my work. I am excited to embark on a journey of learning, growth, and exploration in the world of Web Developing.
+            Welcome to Ray Legal, a prestigious law firm located in Delhi,
+            India. With a team of experienced lawyers, we are dedicated to
+            providing top-notch legal services to our clients. <br />
+            Our commitment to excellence, integrity, and client satisfaction
+            sets us apart in the legal industry. Trust Ray Legal for all your
+            legal needs.
           </p>
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
