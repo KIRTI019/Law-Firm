22c579761de70475797a52886fee5ee5daa355d9
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-03-03 18:15:17 +0530

    Update Nav.jsx

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Nav = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const drawer = (
+      <div className="permalinks">
+        <a href="#">HOME</a>
+        <a href="#about">ABOUT</a>
+        <a href="#services">SERVICES</a>
+        <a href="#contact">CONTACT</a>
+      </div>
+  )
+
   return (
     <nav>
       <div>
@@ -9,13 +25,18 @@ const Nav = () => {
           Ray Legal
         </a>
       </div>
-
       <div className="permalinks">
         <a href="#">HOME</a>
         <a href="#about">ABOUT</a>
         <a href="#services">SERVICES</a>
         <a href="#contact">CONTACT</a>
       </div>
+
+      <button className="draw" onClick={toggleMobileMenu}>{isMobileMenuOpen ? <FaTimes /> : <FaBars />}</button>
+
+      {isMobileMenuOpen && (
+        <drawer />
+      )}
     </nav>
   );
 };
