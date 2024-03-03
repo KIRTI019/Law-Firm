792e92a28f5f2b0aa30987fd04641106a1d51080
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-03-03 12:49:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Nav from "./components/Navbar/Nav";
 import About from "./components/About/about";
 import Services from "./components/Services/services";
 import Testimonial from "./components/Testimonial/testimonial";
-import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
+import Location from "./components/Contact/map";
+import Contact from "./components/Contact/contact";
+import Legal from "./components/Legal/legal";
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
       <Header />
       <Services />
       <About />
+      <Legal />
       <Testimonial />
       <Contact />
       <Footer />
